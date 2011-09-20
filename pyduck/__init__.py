9e53563074e8ecc13aa4db09aff4863eaa868901
Xion <karol.kuczmarski@gmail.com>
2011-09-21 00:27:16 +0200

    Very simple version of InterfaceMeta, checks only names of methods

--- pyduck/__init__.py
+++ pyduck/__init__.py
@@ -1,8 +1,39 @@
+import inspect
 
 
 class InterfaceMeta(type):
     ''' Metaclass for interfaces. '''
-    pass
+    
+    def __new__(meta, class_name, bases, class_dict): #@NoSelf
+        ''' Creates the new interface. '''
+        methods = get_methods(class_dict)
+        iface_dict = build_iface_dict(methods)
+        
+        internals = get_internals(class_dict)
+        iface_dict.update(internals)
+        
+        return type.__new__(meta, class_name, bases, iface_dict)
+    
+    def __instancecheck__(meta, instance): #@NoSelf
+        ''' Custom instance checking. Causes isinstance() to verify interface. '''
+        pass
+        
+
+def get_methods(class_dict):
+    return filter(inspect.isfunction, class_dict.itervalues())
+
+def build_iface_dict(methods):
+    res = {}    # just names for now
+    for m in methods:
+        res[m.func_name] = m
+    return res
+
+def get_internals(class_dict):
+    is_internal = lambda name: name.startswith('__') and name.endswith('__')
+    return dict((k, v) for k, v in class_dict.iteritems() if is_internal(k))
+
+
+###############################################################################
 
 
 class Interface(object):
@@ -11,4 +42,11 @@ class Interface(object):
     
     
 def implements(obj, interface):
-    pass
+    # checking only names of methods now
+    for name, method_obj in interface.__dict__:
+        if name.startswith('_'):   continue
+        obj_member = getattr(obj, name, None)
+        if not (obj_member and inspect.isfunction(obj_member)):
+            return False
+        
+    return True
