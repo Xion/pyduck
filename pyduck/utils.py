f67af0f7060aba678ccf31efa24043e83ddd2acb
Karol Kuczmarski <xion.dev@gmail.com>
2011-09-24 19:00:06 +0200

    Improved the @expects decorator

--- pyduck/utils.py
+++ pyduck/utils.py
@@ -6,8 +6,9 @@ Created on 2011-09-23
 
 @author: xion
 '''
-import inspect
 import functools
+import inspect
+import itertools
 
 
 ###########################################################
@@ -19,11 +20,35 @@ class Any(object):
     pass
 
 
-# Note: currently the check this decorator performs is simple
-# and does not recognize cases where positional arguments
-# (positional wrt to how they interfaces were passed to @expects)
-# has been passed through keywords in the actual function call.
-# This shall be fixed.
+class ArgumentSpec(dict):
+    ''' Slightly customized version of standard Python dictionary
+    that stores the argument specification for a function.
+    It is a mapping of argument indices and names into pyduck interfaces
+    or Python types. It also handles the cases of variadic arguments and
+    keywords arguments if needed.
+    '''
+    def __init__(self, *args, **kwargs):
+        super(ArgumentSpec, self).__init__(*args, **kwargs)
+        self.allows_varargs = False
+        self.allows_kwargs = False
+    
+    def __getitem__(self, key):
+        try:
+            return super(ArgumentSpec, self).__getitem__(key)
+        except IndexError:
+            if isinstance(key, (int, long)):
+                if self.allows_varargs: return Any
+                raise
+            elif isinstance(key, str):
+                if self.allows_kwargs:  return Any
+                raise
+            raise TypeError, "Invalid argument index or name - must be a number or ANSI string"
+            
+    def __setitem__(self, key, value):
+        if not (isinstance(key, (int, long)) or isinstance(key, str)):
+            raise TypeError, "Invalid argument index or name - must be a number or ANSI string"
+        return super(ArgumentSpec, self).__setitem__(key, value)
+            
 
 class ExpectedParametersDecorator(object):
     ''' @expects decorator which can be applied to functions.
@@ -37,44 +62,60 @@ class ExpectedParametersDecorator(object):
         # ...
     '''
     def __init__(self, *args, **kwargs):
-        self.varargs_spec = args
-        self.kwargs_spec = kwargs
+        self.arg_spec = ArgumentSpec()
+        for i, arg in enumerate(args):
+            self.arg_spec[i] = arg
+        self.arg_spec.update(kwargs)
     
     def __call__(self, func):
         is_function = inspect.isfunction(func) or inspect.ismethod(func)
         if not is_function:
             raise TypeError, "@expects can only decorate functions"
         
+        self._improve_argument_spec(func)
+        
         @functools.wraps(func)
         def checked_func(*args, **kwargs):
-            self._check_params_conformance(args, kwargs)
+            self._validate_arguments(args, kwargs)
             return func(*args, **kwargs)
         
         return checked_func
+    
+    def _improve_argument_spec(self, func):
+        ''' Uses the actual arguments of function to improve
+        the argument specification which was saved with the decorator
+        object during its creation. The improved version should work
+        regardless of how the argument was used (positionally or via keyword)
+        in function call.
+        '''
+        arg_names, varargs_name, kwargs_name, _ = inspect.getargspec(func)
+        if inspect.ismethod(func):
+            arg_names = arg_names[1:]   # omit "self"
+            
+        if len(arg_names) > len(self.arg_spec):
+            raise TypeError, "Expected a function with %s argument(s), found only %s" % (len(self.arg_spec), len(arg_names))
         
-    def _check_params_conformance(self, varargs, kwargs):
-        ''' Checks whether specified arguments of function
-        conform with the specification which was passed earlier to the decorator. '''
-        try:
-            for i in xrange(min(len(self.varargs_spec), len(varargs))):
-                actual, expected = varargs[i], self.varargs_spec[i]
-                if expected is Any: continue
-                if not isinstance(actual, expected):
-                    raise TypeError(actual, expected)
-                
-            for k in kwargs:
-                actual, expected = kwargs[k], self.kwargs_spec[k]
-                if expected is Any: continue
-                if not isinstance(actual, expected):
-                    raise TypeError(actual, expected)
-        except TypeError, e:
-            actual, expected = e.args
-            exc_dict = {
-                        'e_type': expected.__name__,
-                        'a_type': type(actual).__name__,
-                        'a_value': actual,
-                        }
-            raise TypeError, "Invalid parameter: expected '%(e_type)s', got '%(a_type)s' (%(a_value)r)" % exc_dict
+        new_spec = ArgumentSpec()
+        if varargs_name:    new_spec.allows_varargs = True
+        if kwargs_name:     new_spec.allows_kwargs = True 
         
+        for i, arg_name in enumerate(arg_names):
+            arg_type = self.arg_spec.get(i) or self.arg_spec.get(arg_name)
+            new_spec[i] = new_spec[arg_name] = arg_type
+            
+        self.arg_spec = new_spec
+        
+    def _validate_arguments(self, varargs, kwargs):
+        ''' Checks whether specified arguments of function conform with
+        the specification which was passed earlier to the decorator. '''
+        all_args = itertools.chain(enumerate(varargs), kwargs.iteritems())
+        for key, arg in all_args:
+            expected = self.arg_spec[key]
+            if expected is Any: continue
+            if not isinstance(arg, expected):
+                expected_type = expected.__name__
+                actual_type = type(arg).__name__
+                raise TypeError, "Invalid argument: expected %s, got %s (%r)" % (expected_type, actual_type, arg)
+            
 
 expects = ExpectedParametersDecorator
