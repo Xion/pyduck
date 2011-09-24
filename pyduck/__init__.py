d48dea5241fda06967aa5d2d513ae625b6a73cfb
Karol Kuczmarski <xion.dev@gmail.com>
2011-09-25 00:35:30 +0200

    Added short description of library to __init__.py

--- pyduck/__init__.py
+++ pyduck/__init__.py
@@ -1,2 +1,9 @@
+'''
+pyduck
+
+@summary: Utility framework for effective use of duck typing via Go-like interfaces
+@author: Karol Kuczmarski "Xion" 
+@license: MIT
+'''
 from interface import InterfaceMeta, Interface, implements
 from helpers import expects, Any
