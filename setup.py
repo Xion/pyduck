c0bfc2a11933f2fa483d0072c25c31111c19bdfd
Karol Kuczmarski <xion.dev@gmail.com>
2011-09-25 22:16:21 +0200

    Fixed setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 
 setup(name = 'pyduck',
-      version = '1.0',
+      version = '0.1',
       description = 'Utility framework for effective use of duck typing via Go-like interfaces',
       long_description = open("README.markdown").read(),
       author = 'Karol Kuczmarski "Xion"',
