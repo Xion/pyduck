32291bbc354335efc3f0f5bfa42a67b906d76546
Karol Kuczmarski <xion.dev@gmail.com>
2011-09-29 19:14:50 +0200

    Increased version number to 0.2 before updating PyPi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 
 setup(name = 'pyduck',
-      version = '0.1',
+      version = '0.2',
       description = 'Utility framework for effective use of duck typing via Go-like interfaces',
       long_description = open("README.markdown").read(),
       author = 'Karol Kuczmarski "Xion"',
