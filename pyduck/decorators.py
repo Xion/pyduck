d9bcd4d08f5980cddd76aea367d2373d7fc0891c
Karol Kuczmarski <xion.dev@gmail.com>
2011-11-16 15:42:26 +0100

    @expects implemented in terms of InspectArgumentsDecorator

--- pyduck/decorators.py
+++ pyduck/decorators.py
@@ -137,7 +137,7 @@ class ArgumentError(TypeError):
         self.argument = argument
         self.expected = expected
 
-class ExpectedParametersDecorator(object):
+class ExpectedParametersDecorator(InspectArgumentsDecorator):
     ''' @expects decorator which can be applied to functions.
     It performs a check whether function's actual parameters
     match the specification (in terms of pyduck interfaces)
@@ -148,73 +148,26 @@ class ExpectedParametersDecorator(object):
     def write(value, dest_file):
         # ...
     '''
-    def __init__(self, *args, **kwargs):
-        self.arg_spec = ArgumentSpec()
-        for i, arg in enumerate(args):
-            self.arg_spec[i] = arg
-        self.arg_spec.update(kwargs)
-        self.omit_self = False  # we don't know yet what function we'll decorate
-    
     def __call__(self, func):
         if not inspect.isroutine(func):
             raise TypeError, "@expects can only decorate functions"
         
-        self._improve_argument_spec(func)
-        
-        @functools.wraps(func)
-        def checked_func(*args, **kwargs):
-            self._validate_arguments(args, kwargs)
-            return func(*args, **kwargs)
-        
+        checked_func = super(ExpectedParametersDecorator, self).__call__(func)
+
         checked_func = transfer_specs(func, checked_func)
         checked_func._arguments = self.arg_spec
         checked_func._has_self = self.omit_self
         return checked_func
-    
-    def _improve_argument_spec(self, func):
-        ''' Uses the actual arguments of function to improve
-        the argument specification which was saved with the decorator
-        object during its creation. The improved version should work
-        regardless of how the argument was used (positionally or via keyword)
-        in function call.
-        '''
-        arg_names, varargs_name, kwargs_name, _ = inspect.getargspec(func)
-        
-        first_arg_is_self = len(arg_names) > 0 and arg_names[0] == 'self'
-        is_unbound_method = not inspect.ismethod(func) and first_arg_is_self
-        if is_unbound_method:
-            self.omit_self = True
-        if first_arg_is_self:
-            arg_names = arg_names[1:]   # 'self' is not part of argument spec
 
-        if len(arg_names) > len(self.arg_spec):
-            raise TypeError("Expected a function with %s argument(s), found only %s"
-                            % (len(self.arg_spec), len(arg_names)))
-         
-        for i, arg_name in enumerate(arg_names):
-            arg_type = self.arg_spec.get(i) or self.arg_spec.get(arg_name)
-            self.arg_spec[i] = arg_type
-            self.arg_spec[arg_name] = arg_type
-            
-        if varargs_name:    self.arg_spec.allows_varargs = True
-        if kwargs_name:     self.arg_spec.allows_kwargs = True
-        
-    def _validate_arguments(self, varargs, kwargs):
-        ''' Checks whether specified arguments of function conform with
-        the specification which was passed earlier to the decorator. '''
-        if self.omit_self:
-            varargs = varargs[1:]   # omit 'self'
-            
-        all_args = itertools.chain(enumerate(varargs), kwargs.iteritems())
-        for key, arg in all_args:
-            expected = self.arg_spec[key]
-            if expected is Any: continue
-            if not self._is(arg, expected):
-                expected_type = expected.__name__
-                actual_type = type(arg).__name__
-                raise ArgumentError("Invalid argument: expected %s, got %s (%r)" % (expected_type, actual_type, arg),
-                                    argument = arg, expected = expected)
-            
+    def _process_argument(self, expected, actual):
+        is_ok = expected is Any or self._is(actual, expected)
+        if not is_ok:
+            expected_type = expected.__name__
+            actual_type = type(actual).__name__
+            raise ArgumentError("Invalid argument: expected %s, got %s (%r)" % (expected_type, actual_type, actual),
+                                argument = arg, expected = expected)
+        return actual
+    
     @staticmethod
     def _is(obj, type_):
         ''' Generalized functions for checking whether an object
